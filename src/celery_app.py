daaff4e1c13d90248235467f099e96dc4c285f9e
Kepins <sz.kepinski1@gmail.com>
2024-12-08 20:00:51 +0100

    WIP-Redis lock

--- src/celery_app.py
+++ src/celery_app.py
@@ -1,24 +1,40 @@
+import time
+
 import torch
 from celery import Celery
+from redis import Redis
 from transformers import AutoTokenizer, LlamaForCausalLM, pipeline
-
 
 celery_app = Celery(__name__)
 
 
 @celery_app.task()
-def inference(prompt):
-    model_id = './Llama-3.2-1B'
-    model = LlamaForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16)
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
+def inference():
+    redis = Redis(host='redis', port=6379, db=0)
+    lock_acquired = redis.set("GENERATE_NEXT_TOKENS", "locked", nx=True, ex=60)
+    if not lock_acquired:
+        print("LOCK NOT ACQUIRED")
+        return
 
-    pipe = pipeline(
-        'text-generation',
-        model=model,
-        tokenizer=tokenizer,
-        device=0,
-        max_new_tokens=1,  # Limit to one token
-    )
-    responses = pipe(prompt)
-    next_token = responses[0]['generated_text'].replace(prompt, '').strip()
-    return next_token
+    prompts = redis.lpop("PROMPT_QUEUE", 2)
+
+    time.sleep(2)
+
+    print(prompts)
+
+    # model_id = './Llama-3.2-1B'
+    # model = LlamaForCausalLM.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+    # tokenizer = AutoTokenizer.from_pretrained(model_id)
+    #
+    # pipe = pipeline(
+    #     'text-generation',
+    #     model=model,
+    #     tokenizer=tokenizer,
+    #     device=0,
+    #     max_new_tokens=1,  # Limit to one token
+    # )
+    # responses = pipe(prompt)
+    # next_token = responses[0]['generated_text'].replace(prompt, '').strip()
+
+    redis.delete("GENERATE_NEXT_TOKENS")
+    return "next_token"
--- src/main.py
+++ src/main.py
@@ -1,11 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from redis.asyncio import Redis
 
 from .routers import example
 
+context = {}
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    context["redis"] = Redis(host='redis', port=6379, db=0)
+    yield
+    await context["redis"].close()
 app = FastAPI(
     title='Word prediction service',
     description='Word prediction service using LLM',
     version='0.1',
+    lifespan=lifespan,
 )
 
 app.include_router(example.router)
